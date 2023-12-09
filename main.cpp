b222fe1306304e0916805372014308d8b04ff01e
TheAnushervon <anushervon4j@gmail.com>
2023-12-09 20:07:51 +0300

    accomplished 1,2,5.wrong 3.write 8

--- main.cpp
+++ main.cpp
@@ -10,9 +10,12 @@ double epsilon = numeric_limits<double>::epsilon();
 vector<double> xi_val = {0};
 vector<double> yi_val = {0};
 vector<double> euler_yi;
-vector<double> ieuler_yi; 
-vector<double> k1_i; 
-vector<double> k2_i; 
+vector<double> ieuler_yi;
+vector<double> k1_i;
+vector<double> k2_i;
+vector<double> euler_le_xi;
+vector<double> ni_val; 
+
 double h;
 double pi = 2 * acos(0.0);
 
@@ -56,15 +59,7 @@ void print_yi(int n)
         cout << fixed << setprecision(5) << yi_val[i] << " ";
     }
 }
-void variant_first(int n)
-{
-    xi_calc(n);
-    print_xi(n);
 
-    cout << endl;
-    yi_calc(n);
-    print_yi(n);
-}
 double func_calc(double xi_val, double yi_val)
 {
     double result;
@@ -81,24 +76,31 @@ void calc_Euler_yi(int n)
         euler_yi.push_back(val);
     }
 }
-void calc_k1(vector<double> xi_val, vector<double> yi_val, int n){
-    for (int i = 0 ; i < n ; i++) { 
-       k1_i.push_back(func_calc(xi_val[i], yi_val[i])); 
+void calc_k1(vector<double> xi_val, vector<double> yi_val, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        k1_i.push_back(func_calc(xi_val[i], yi_val[i]));
     }
 }
-void calc_k2(vector<double> xi_val, vector<double> yi_val, int n){
-    for (int i = 0 ; i < n ; i++) { 
-       k2_i.push_back(func_calc(xi_val[i]+h, (yi_val[i]+h*k1_i[i]))); 
+void calc_k2(vector<double> xi_val, vector<double> yi_val, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        k2_i.push_back(func_calc(xi_val[i] + h, (yi_val[i] + h * k1_i[i])));
     }
 }
-void calc_iEuler_yi(int n) { 
-    calc_k1(xi_val, yi_val, n) ; 
-    calc_k2(xi_val, yi_val, n) ;
-    ieuler_yi.push_back(yi_val[0]+(h/2)*(k1_i[0] + k2_i[0]));  
-    double result ; 
-    for (int i = 0 ; i < n-1; i++) { 
-        result = ieuler_yi[i] + (h/2)*(k1_i[i]*k2_i[i]); 
-        ieuler_yi.push_back(result) ; 
+// problem with k1[i] and k2[i] - out of range
+void calc_iEuler_yi(int n)
+{
+    calc_k1(xi_val, yi_val, n);
+    calc_k2(xi_val, yi_val, n);
+    ieuler_yi.push_back(yi_val[0] + (h / 2) * (k1_i[0] + k2_i[0]));
+    double result;
+    for (int i = 0; i < n - 1; i++)
+    {
+        result = ieuler_yi[i] + (h / 2) * (k1_i[i] * k2_i[i]);
+        ieuler_yi.push_back(result);
     }
 }
 void print_Euler_yi(int n)
@@ -109,11 +111,38 @@ void print_Euler_yi(int n)
         cout << fixed << setprecision(5) << euler_yi[i] << " ";
     }
 }
-void print_iEuler_yi(int n) { 
-    cout << "iEuler_yi=\n"; 
-    for (int i = 0 ; i < n ; i++) { 
-        cout << fixed<< setprecision(5) << ieuler_yi[i] << " "; 
+void print_iEuler_yi(int n)
+{
+    cout << "iEuler_yi=\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << fixed << setprecision(5) << ieuler_yi[i] << " ";
     }
+}
+void calc_Euler_LE_xi(int n)
+{
+    yi_calc(n);
+    calc_Euler_yi(n) ; 
+    vector<double> check ;  
+    for (int i = 0; i < n; i++)
+    {  
+        euler_le_xi.push_back(abs(euler_yi[i]- yi_val[i]));
+    }
+}
+void print_Euler_LE_xi(int n) { 
+        cout << "Euler_LE(xi)=\n"; 
+    for (int i = 0 ;i < n ; i++) {
+        cout << euler_le_xi[i] << " " ; 
+    }
+}
+void variant_first(int n)
+{
+    xi_calc(n);
+    print_xi(n);
+
+    cout << endl;
+    yi_calc(n);
+    print_yi(n);
 }
 void variant_second(int n)
 {
@@ -123,12 +152,37 @@ void variant_second(int n)
     calc_Euler_yi(n);
     print_Euler_yi(n);
 }
-void variant_third(int n){ 
-xi_calc(n) ; 
-print_xi(n) ; 
-cout << endl ; 
-calc_iEuler_yi(n);
-print_iEuler_yi(n) ; 
+void print_ni(int n) {
+    cout << "ni=\n" ; 
+    for (int i = 0 ; i <= n; i++) { 
+        cout << ni_val[i] << " " ; 
+    }
+}
+void calc_ni (int n, int n1, int n2) { 
+    for (int i = 0; i <= n; i++) { 
+        ni_val.push_back(n1+i); 
+    }
+}
+void variant_third(int n)
+{
+    xi_calc(n);
+    print_xi(n);
+    cout << endl;
+    // problem in this funciton
+    calc_iEuler_yi(n);
+    print_iEuler_yi(n);
+}
+void variant_fifth(int n)
+{
+    xi_calc(n);
+    print_xi(n);
+    cout << endl;
+    calc_Euler_LE_xi(n);
+    print_Euler_LE_xi(n);
+}
+void variant_eighth(int n, int n1, int n2) { 
+    calc_ni(n, n1, n2);
+    print_ni(n) ; 
 }
 int main()
 {
@@ -148,8 +202,15 @@ int main()
     case 2:
         variant_second(n);
         break;
-    case 3: 
-        variant_third(n); 
-        break; 
+    case 3:
+        variant_third(n); // wrong
+        break;
+    case 4:
+        break;
+    case 5:
+        variant_fifth(n);
+        break;
+    case 8: 
+        variant_eighth(n, n1, n2) ; // not finished
     }
 }
