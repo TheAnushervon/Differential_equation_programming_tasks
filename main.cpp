37b4de70797defdaaebe98ae43a907f315df092c
TheAnushervon <anushervon4j@gmail.com>
2023-12-11 00:24:00 +0300

    fully working 1.1

--- main.cpp
+++ main.cpp
@@ -14,11 +14,16 @@ vector<double> euler_yi;
 vector<double> ieuler_yi;
 vector<double> k1_i;
 vector<double> k2_i;
+vector<double> k3_i; 
+vector<double> k4_i; 
 vector<double> euler_le_xi;
 vector<double> ni_val; 
 vector<double> impr_euler_le_xi; 
 vector<double> euler_global_error; 
 vector<double> euler_improved_global_error; 
+vector<double> runge_kutta_sol; 
+vector<double>runge_kutta_local_errors; 
+vector<double> runge_kutta_global_errors; 
 double h;
 double pi = 2 * acos(0.0);
 
@@ -94,6 +99,24 @@ void calc_k2(vector<double> xi_val, vector<double> yi_val, int n)
         k2_i.push_back(func_calc(xi_val[i] + h, (yi_val[i] + h * k1_i[i])));
     }
 }
+
+void calc_k3(vector<double> xi_val, vector<double> yi_val, int n) {
+   
+    for (int i = 0; i < n; i++)
+    {
+        k2_i.push_back(func_calc(xi_val[i] + (h/2), (yi_val[i] + (h/2) * k1_i[i])));
+    }
+
+    double h_div = h/2 ; 
+    for (int i = 0 ;i < n ; i ++) {
+        k3_i.push_back(func_calc(xi_val[i]+h_div, yi_val[i]+h_div*k2_i[i])); 
+    }
+}
+void calc_k4(vector<double> xi_val, vector<double> yi_val, int n) { 
+    for (int i = 0 ; i < n; i++) { 
+        k4_i.push_back(func_calc(xi_val[i]+h, yi_val[i]+h*k3_i[i])); 
+    }
+}
 // problem with k1[i] and k2[i] - out of range
 void calc_iEuler_yi(int n)
 {
@@ -152,6 +175,19 @@ void calc_imprEuler_LE_xi(int n)
         impr_euler_le_xi.push_back(abs(ieuler_yi[i]- yi_val[i]));
     }
 }
+void calc_runge_kutta_sol(int n){
+    yi_calc(n); 
+    calc_k1(xi_val, yi_val, n); 
+    calc_k3(xi_val, yi_val, n); 
+    calc_k4(xi_val, yi_val, n); 
+    runge_kutta_sol.push_back(yi_val[0]); 
+    double rks = 0 ; 
+    for (int i = 0 ; i < n-1; i++) { 
+        rks = runge_kutta_sol[i] + (h/6)*(k1_i[i]+2*k2_i[i]+ 2*k3_i[i]+k4_i[i]); 
+        runge_kutta_sol.push_back(rks); 
+    }
+    
+}
 void print_Euler_LE_xi(int n) { 
         cout << "Euler_LE(xi)=\n"; 
     for (int i = 0 ;i < n ; i++) {
@@ -201,6 +237,24 @@ void variant_third(int n)
     calc_iEuler_yi(n);
     print_iEuler_yi(n);
 }
+void variant_fourth(int n) { 
+    xi_calc(n) ; 
+    print_xi(n); 
+    cout << endl ; 
+
+    calc_runge_kutta_sol(n); 
+    cout << "RK4_yi=\n"; 
+    for (int i = 0; i < n; i++){
+        cout << runge_kutta_sol[i]<< " " ; 
+    }
+}
+void calc_RK_Local_error(int n){
+    //xi_calc(n); 
+    calc_runge_kutta_sol(n); 
+    for (int i = 0 ;i < n; i++) {
+        runge_kutta_local_errors.push_back(abs(runge_kutta_sol[i]-yi_val[i])); 
+    }
+}
 void variant_fifth(int n)
 {
     xi_calc(n);
@@ -215,6 +269,17 @@ void variant_six(int n) {
     cout << endl ; 
     calc_imprEuler_LE_xi( n) ; 
     print_imprEuler_LE_xi(n) ; 
+}
+void variant_seven(int n){
+    xi_calc(n); 
+    print_xi(n); 
+    cout << endl ; 
+    calc_RK_Local_error(n);
+    cout << "RK4_LE(xi)=\n";  
+    for (int i = 0 ;i < n ; i++){
+        cout << runge_kutta_local_errors[i]<< " "; 
+    }
+
 }
 void variant_eighth(int n, int n1, int n2) { 
     calc_ni(n, n1, n2);
@@ -266,7 +331,33 @@ void variant_nine(int n, int n1, int n2) {
         cout<< fixed<< setprecision(5) << euler_improved_global_error[i] << " " ; 
     }
 }
-
+void variant_ten(int n, int n1, int n2) { 
+    calc_ni(n,n1,n2); 
+    print_ni(n); 
+    cout << endl; 
+    
+    runge_kutta_global_errors.resize(n+1); 
+    for (int i = 0 ; i < n+1;i++){
+        xi_calc(ni_val[i]); 
+        calc_RK_Local_error(ni_val[i]); 
+       
+        runge_kutta_global_errors[i] = (*max_element(runge_kutta_local_errors.begin(), runge_kutta_local_errors.end())); 
+        xi_val.clear(); 
+        yi_val.clear(); 
+        runge_kutta_sol.clear(); 
+        runge_kutta_local_errors.clear(); 
+        k1_i.clear(); 
+        k2_i.clear();
+        k3_i.clear(); 
+        k4_i.clear();  
+        xi_val.push_back(0); 
+        yi_val.push_back(0); 
+    }
+    cout << "RK4_GE(n)=\n"; 
+    for (int i = 0 ; i < runge_kutta_global_errors.size() ;i++){
+        cout << fixed<<setprecision(5)<<runge_kutta_global_errors[i]<< " "; 
+    }
+}
 int main()
 {
 
@@ -279,29 +370,35 @@ int main()
     switch (number_task)
     {
     case 1:
-        variant_first(n); // ok
+        variant_first(n); 
         break;
 
     case 2:
-        variant_second(n); // ok 
+        variant_second(n);  
         break;
     case 3:
-        variant_third(n); // ok
+        variant_third(n); 
         break;
     case 4:
+        variant_fourth(n); 
         break;
     case 5:
-        variant_fifth(n); // ok 
+        variant_fifth(n);  
         break;
     case 6: 
-        variant_six(n) ;  // ok
+        variant_six(n) ;  
         break; 
+    case 7: 
+        variant_seven(n);  
+        break;
     case 8: 
-        variant_eighth(n, n1, n2) ;  // ok 
+        variant_eighth(n, n1, n2) ;   
         break; 
     case 9:
-        variant_nine(n, n1, n2) ; 
+        variant_nine(n, n1, n2); 
         break; 
+    case 10: 
+        variant_ten(n, n1, n2);  
 
     }
 }
