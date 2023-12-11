bcc453ded48b7203ac7ff3cb360b06a9b222b457
TheAnushervon <anushervon4j@gmail.com>
2023-12-11 03:42:34 +0300

    with errors

--- main2.cpp
+++ main2.cpp
@@ -12,6 +12,12 @@ vector<double> k1_for_xi;
 vector<double> k1_for_yi; 
 vector<double> k2_for_xi; 
 vector<double> k2_for_yi; 
+vector<double> k3_for_xi; 
+vector<double> k3_for_yi; 
+vector<double> k4_for_xi;
+vector<double> k4_for_yi;  
+vector<double> runge_kutta_for_xi; 
+vector<double> runge_kutta_for_yi; 
 double pi = 2 * acos(0.0);
 const double t0= 0 ; 
 const double b = pi;  
@@ -52,19 +58,42 @@ void calc_k2(int i){
         k2_for_yi.push_back(func_calc(val_ti[i+1], val_xi[i]+h*k1_for_xi[i], val_yi[i]+h*k1_for_yi[i], 'y')); 
 
 }
+void calc_k3(int i){
+k3_for_xi.push_back(func_calc(val_ti[i+1], val_xi[i]+h/2+k2_for_xi[i],val_yi[i]+h/2+k2_for_yi[i],'x')); 
+k3_for_yi.push_back(func_calc(val_ti[i+1], val_xi[i]+h/2+k2_for_xi[i],val_yi[i]+h/2+k2_for_yi[i],'y')) ; 
+}
+void calc_k4(int i){
+k4_for_xi.push_back(func_calc(val_ti[i+1], val_xi[i]+h*k3_for_xi[i], val_yi[i]+h*k3_for_yi[i],'x')); 
+k4_for_yi.push_back(func_calc(val_ti[i+1], val_xi[i]+h*k3_for_xi[i], val_yi[i]+h*k3_for_yi[i],'y')); 
+
+}
 void calc_improved_euler_for_xi_and_yi(){
-    improved_value_xi.push_back(1); 
-    improved_value_yi.push_back(2); 
     double h_div = h/2 ; 
     for (int  i= 0 ; i < n-1 ; i ++) { 
         calc_k1(i); 
         calc_k2(i);
 
-        double value_for_xi = improved_value_xi[i]+h_div*(k1_for_xi[i]+k2_for_xi[i]); 
-        double value_for_yi = improved_value_yi[i]+h_div*(k1_for_yi[i]+k2_for_yi[i]); 
-        improved_value_xi.push_back(value_for_xi); 
-        improved_value_yi.push_back(value_for_yi); 
+        double value_for_xi = val_xi[i]+h_div*(k1_for_xi[i]+k2_for_xi[i]); 
+        double value_for_yi = val_yi[i]+h_div*(k1_for_yi[i]+k2_for_yi[i]); 
+        val_xi.push_back(value_for_xi); 
+        val_yi.push_back(value_for_yi); 
     }
+}
+void calc_runge_kutta_for_xi_and_yi(){
+runge_kutta_for_xi.push_back(val_xi[0]); 
+runge_kutta_for_yi.push_back(val_yi[0]); 
+for (int i = 0; i<n-1;i++){
+    calc_k1(i); 
+    calc_k2(i); 
+    calc_k3(i); 
+    calc_k4(i); 
+    double result_for_xi = 
+    runge_kutta_for_xi[i]+h/6*(k1_for_xi[i] + 2*k2_for_xi[i]+2*k3_for_xi[i]+k4_for_xi[i]); 
+    double result_for_yi = 
+    runge_kutta_for_yi[i]+h/6*(k1_for_yi[i]+ 2*k2_for_yi[i]+2*k3_for_yi[i]+k4_for_yi[i]); 
+    runge_kutta_for_xi.push_back(result_for_xi); 
+    runge_kutta_for_yi.push_back(result_for_yi); 
+}
 }
 void print_ti(){
     cout << "ti=\n"; 
@@ -86,16 +115,30 @@ void print_euler(char which_points){
         }
     }
 }
+void print_runge_kutta(char which_output){
+    if (which_output == 'x'){
+        cout << "RK4_xi=\n"; 
+        for (int i = 0 ;i < runge_kutta_for_xi.size() ; i++) { 
+            cout << runge_kutta_for_xi[i]<< " "; 
+        }
+    }
+    else if (which_output == 'y'){
+        cout << "RK4_yi=\n"; 
+        for (int i = 0 ;i <runge_kutta_for_yi.size();i++){
+            cout<< runge_kutta_for_yi[i]<< " "; 
+        }
+    }
+}
 void print_improved_euler(char which_output){
     if (which_output == 'x'){
         cout << "iEuler_xi=\n"; 
         for (int i = 0 ;i < n ; i++) { 
-            cout<< improved_value_xi[i] << " "; 
+            cout<< val_xi[i] << " "; 
         }
     }else {
         cout << "iEuler_yi=\n"; 
         for (int i = 0 ; i< n ;i++){
-            cout << improved_value_yi[i]<< " "; 
+            cout << val_yi[i]<< " "; 
         }
     }
 }
@@ -120,6 +163,13 @@ cout << endl;
 print_improved_euler('y'); 
 }
 void third_task() { 
+calc_ti(); 
+print_ti(); 
+cout << endl; 
+calc_runge_kutta_for_xi_and_yi(); 
+print_runge_kutta('x'); 
+cout << endl ;
+print_runge_kutta('y'); 
 
 }
 int main() { 
